e5425eabc16dc2ce36f5dd987429eb8037e08664
Dev Code <hellodev_code@outlook.com>
2024-07-03 22:36:43 +0530

    3 July

---
+++ rating_900/Problem1878C.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Problem1878C {
+    private static void solve(Scanner sc) {
+        long n = sc.nextLong(); 
+        long k = sc.nextLong(); 
+        long x = sc.nextLong();
+        
+        long min = k * (k + 1) / 2;
+        long max = (n * (n + 1) - (n - k) * (n - k + 1)) / 2;
+        
+        if ( x >= min &&  x <= max) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            solve(sc);
+        }
+        sc.close();
+    }
+}
---
+++ rating_900/Problem1883B.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Problem1883B {
+    // bacacd
+    private static void solve(Scanner sc) {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        Map<Character, Integer> map = new HashMap<>();
+        int char_with_odd_occurence = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (Character ch : map.keySet()) {
+            if (map.get(ch) % 2 != 0)
+                char_with_odd_occurence++;
+        }
+        if (char_with_odd_occurence - k > 1) 
+            System.out.println("NO");
+        else
+            System.out.println("YES");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            solve(sc);
+        }
+        sc.close();
+    }
+}
