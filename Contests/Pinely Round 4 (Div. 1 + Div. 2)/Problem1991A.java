8de2acdc7e62299e8ebe9a915ac2b789ada4ab9b
Dev Code <hellodev_code@outlook.com>
2024-07-29 19:35:52 +0530

    28 July

---
+++ Contests/Pinely Round 4 (Div. 1 + Div. 2)/Problem1991A.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Problem1991A {
+    static int n;
+    static int a[];
+
+    public static void solve(Scanner sc) {
+        n = sc.nextInt();
+        a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        if (n == 1) {
+            System.out.println(a[0]);
+            return;
+        } else {
+            int ans = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                if ((n - 1 - i) % 2 == 0) {
+                    ans = Math.max(ans, a[i]);
+                }
+            }
+            System.out.println(ans);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            solve(sc);
+        }
+        sc.close();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // }
+}
