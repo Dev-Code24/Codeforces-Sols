f670e5cfc1eb7217347b07d35a6e8b7818913405
Dev Code <hellodev_code@outlook.com>
2024-08-22 15:12:57 +0530

    20 Aug -- modified 22 Aug

--- Contests/Codeforces Round 967 (Div. 2)/Problem2001D.java
+++ Contests/Codeforces Round 967 (Div. 2)/Problem2001D.java
@@ -1,16 +1,50 @@
 import java.util.*;
 
+// if not understood, watch this: https://www.youtube.com/watch?v=MqRyqCoi6Lc&t=3622s
 public class Problem2001D {
-    static int n;
-    static int a[];
-
     public static void solve(Scanner sc) {
-        n = sc.nextInt();
-        a = new int[n];
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] count = new int[n + 1];
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
+            count[a[i]]++;
         }
+        
+        boolean[] b = new boolean[n + 1];
+        int[] ans = new int[n];
+        int k = 0;
 
+        for (int i = 0; i < n; i++) {
+            int x = a[i];
+            count[x]--;
+            if (b[x])
+                continue;
+            while (true) {
+                if (k >= 1 && count[ans[k - 1]] > 0 && better(ans, x, k - 1)) {
+                    b[ans[k - 1]] = false;
+                    k--;
+                } else if (k >= 2 && count[ans[k - 1]] > 0 && count[ans[k - 2]] > 0 && better(ans, x, k - 2)) {
+                    b[ans[k - 2]] = b[ans[k - 1]] = false;
+                    k -= 2;
+                } else {
+                    break;
+                }
+            }
+            ans[k++] = x;
+            b[x] = true;
+        }
+        System.out.println(k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static boolean better(int[] ans, int x, int i) {
+        if (i % 2 == 0)
+            return x > ans[i];
+        return x < ans[i];
     }
 
     public static void main(String[] args) {
