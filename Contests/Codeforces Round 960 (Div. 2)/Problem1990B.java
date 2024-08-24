f72a06556c84286a4a4ab89d18011d9c4ee9cac6
Dev Code <hellodev_code@outlook.com>
2024-08-24 13:48:00 +0530

    24 Aug

--- Contests/Codeforces Round 960 (Div. 2)/Problem1990B.java
+++ Contests/Codeforces Round 960 (Div. 2)/Problem1990B.java
@@ -1,26 +1,33 @@
 import java.util.*;
 
 public class Problem1990B {
+    static int n, x, y;
+    static int a[];
+
     public static void solve(Scanner sc) {
-        int n = sc.nextInt();
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        n = sc.nextInt();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        x--;
+        y--;
 
-        int a[] = new int[n];
-        for (int i = 0; i < y; i++) {
-            a[i] = -1;
+        a = new int[n];
+        int curr = -1;
+        for (int i = y - 1; i >= 0; i--) {
+            a[i] = curr;
+            curr = -curr;
+        }
+        curr = -1;
+        for (int i = x + 1; i < n; i++) {
+            a[i] = curr;
+            curr = -curr;
         }
 
-        for (int i = y; i < x; i++) {
+        for (int i = y; i <= x; i++) {
             a[i] = 1;
         }
-
-        for (int i = x; i < n; i++) {
-            a[i] = 1;
-        }
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
+        for (Integer el : a) {
+            System.out.print(el + " ");
         }
         System.out.println();
 
