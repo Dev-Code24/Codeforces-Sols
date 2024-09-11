5af15058886cb60008853557b19f0d9059482f80
Dev Code <hellodev_code@outlook.com>
2024-09-11 20:57:42 +0530

    11 Sept

---
+++ rating_1200/Problem1520D.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Problem1520D {
+    static int n, a[];
+
+    public static void solve(Scanner sc) {
+        n = sc.nextInt();
+        a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // aj - ai = j - i
+
+        var map = new TreeMap<Integer, Integer>();
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += map.getOrDefault(i - a[i], 0);
+            map.put(i - a[i], map.getOrDefault(i - a[i], 0) + 1);
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            solve(sc);
+        }
+        sc.close();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // @Override
+    // public boolean equals(Object o) {
+    // if (this == o)
+    // return true;
+    // if (o == null || getClass() != o.getClass())
+    // return false;
+    // Pair pair = (Pair) o;
+    // return first == pair.first && second == pair.second;
+    // }
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(first, second);
+    // }
+    // }
+}
