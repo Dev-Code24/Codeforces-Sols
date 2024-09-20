ef1b1d1d8902d5d1033c2472778d1077a649b0aa
Dev Code <hellodev_code@outlook.com>
2024-09-20 22:05:53 +0530

    20 Sept

---
+++ Contests/Codeforces Round 973 (Div. 2)/Problem2013A.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Problem2013A {
+    static int n;
+    static int x, y;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            n = sc.nextInt(); // total fruits
+            x = sc.nextInt(); // blender can mix this much fruits per sec
+            y = sc.nextInt(); // can put this much fruits per sec
+
+            System.out.println((long) Math.ceilDiv(n, Math.min(x, y)));
+
+        }
+        sc.close();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // @Override
+    // public boolean equals(Object o) {
+    // if (this == o)
+    // return true;
+    // if (o == null || getClass() != o.getClass())
+    // return false;
+    // Pair pair = (Pair) o;
+    // return first == pair.first && second == pair.second;
+    // }
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(first, second);
+    // }
+    // }
+}
