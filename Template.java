f63a82b281feb0abd3f7021e8167180eb9170387
Dev Code <hellodev_code@outlook.com>
2024-09-24 13:30:20 +0530

    added my current template

---
+++ Template.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Template {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine()); // use this everytime when taking input or changing line of input
+            n = Integer.parseInt(st.nextToken());
+            // Add your logic here
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // @Override
+    // public boolean equals(Object o) {
+    // if (this == o)
+    // return true;
+    // if (o == null || getClass() != o.getClass())
+    // return false;
+    // Pair pair = (Pair) o;
+    // return first == pair.first && second == pair.second;
+    // }
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(first, second);
+    // }
+    // }
+}
