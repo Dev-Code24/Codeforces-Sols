7a21aad5b940c26082f3061964070ef5c0aeeefc
Dev Code <hellodev_code@outlook.com>
2024-09-29 17:58:18 +0530

    29 Sept

---
+++ rating_1300/Problem1366B.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem1366B {
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+    static int n, x, m;
+    static int a[];
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int T = Integer.parseInt(nextToken());
+        while (T-- > 0) {
+            n = Integer.parseInt(nextToken());
+            x = Integer.parseInt(nextToken());
+            m = Integer.parseInt(nextToken());
+
+            int l = x, r = x;
+            for (int i = 0; i < m; i++) {
+                int ll = Integer.parseInt(nextToken());
+                int rr = Integer.parseInt(nextToken());
+                if (ll > r || rr < l)
+                    continue;
+                l = Math.min(ll, l);
+                r = Math.max(rr, r);
+            }
+
+            out.println(r - l + 1);
+
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // @Override
+    // public boolean equals(Object o) {
+    // if (this == o)
+    // return true;
+    // if (o == null || getClass() != o.getClass())
+    // return false;
+    // Pair pair = (Pair) o;
+    // return first == pair.first && second == pair.second;
+    // }
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(first, second);
+    // }
+    // }
+}
