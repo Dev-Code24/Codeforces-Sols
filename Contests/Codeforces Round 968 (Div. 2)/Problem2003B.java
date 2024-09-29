ff0696b7396e85370dc33ebb649bece3f6ed93d8
Dev Code <hellodev_code@outlook.com>
2024-09-29 10:45:56 +0530

    25 Aug

---
+++ Contests/Codeforces Round 968 (Div. 2)/Problem2003B.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Problem2003B {
+    static int n;
+    static int a[];
+
+    public static void solve(Scanner sc) {
+        n = sc.nextInt();
+        a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        System.out.println(a[n/2]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            solve(sc);
+        }
+        sc.close();
+    }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // }
+}
+
+// 2, 2, 2, 5, 5, 7, 7, 9, 10, 10
