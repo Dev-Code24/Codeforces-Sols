df405a0e7fd0a0f8847c7eddf244be59b371f5a7
Dev Code <hellodev_code@outlook.com>
2024-10-03 23:33:58 +0530

    Updated My Template

--- Template.java
+++ Template.java
@@ -1,18 +1,18 @@
 import java.io.*;
 import java.util.*;
+// import java.math.*;
 
 public class Template {
     static BufferedReader br;
     static PrintWriter out;
     static StringTokenizer st;
-    static int n;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         out = new PrintWriter(new BufferedOutputStream(System.out));
         int T = Integer.parseInt(nextToken());
         while (T-- > 0) {
-            n = Integer.parseInt(nextToken());
+            int n = Integer.parseInt(nextToken());
             // Add logic here
         }
         out.flush();
@@ -20,12 +20,16 @@ public class Template {
         br.close();
     }
 
-    static String nextToken() throws IOException {
+    private static String nextToken() throws IOException {
         while (st == null || !st.hasMoreTokens()) {
             st = new StringTokenizer(br.readLine());
         }
         return st.nextToken();
     }
+
+    // private static long sqrt(long x) {
+    // return new BigInteger(String.valueOf(x)).sqrt().longValue();
+    // }
 
     // private static long gcd(long a, long b) {
     // while (b != 0) {
