5d6f0ad22e2658a665a065f95950386cf6f21be1
Dev Code <hellodev_code@outlook.com>
2024-10-13 21:11:43 +0530

    13 Oct

---
+++ rating_1200/Problem1975C.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem1975C {
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int T = Integer.parseInt(nextToken());
+        while (T-- > 0) {
+            int N = Integer.parseInt(nextToken());
+            int[] A = new int[N + 2];
+            for (int i = 0; i < N; i++) {
+                int a = Integer.parseInt(nextToken());
+                A[i] = (a);
+            }
+
+            if (N == 2) {
+                out.println(Math.min(A[0], A[1]));
+            } else {
+                int ans = Math.min(A[0], A[1]);
+                for (int i = 0; i < N - 1; i++) {
+                    var b = new ArrayList<Integer>();
+                    for (int j = 0; j <= 2; j++)
+                        b.add(A[i + j]);
+                    Collections.sort(b);
+                    ans = Math.max(ans, b.get(1));
+                }
+                out.println(ans);
+            }
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    private static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    // private static long sqrt(long x) {
+    // return new BigInteger(String.valueOf(x)).sqrt().longValue();
+    // }
+
+    // private static long gcd(long a, long b) {
+    // while (b != 0) {
+    // long temp = b;
+    // b = a % b;
+    // a = temp;
+    // }
+    // return a;
+    // }
+
+    // private static long lcm(long a, long b) {
+    // return a * b / gcd(a, b);
+    // }
+
+    // static class Pair implements Comparable<Pair> {
+    // long first;
+    // long second;
+    //
+    // Pair(long _first, long _second) {
+    // this.first = _first;
+    // this.second = _second;
+    // }
+    // @Override
+    // public int compareTo(Pair o) {
+    // return Long.compare(this.first, o.first);
+    // }
+    // @Override
+    // public String toString() {
+    // return "(" + first + ',' + second + ")";
+    // }
+    // @Override
+    // public boolean equals(Object o) {
+    // if (this == o)
+    // return true;
+    // if (o == null || getClass() != o.getClass())
+    // return false;
+    // Pair pair = (Pair) o;
+    // return first == pair.first && second == pair.second;
+    // }
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(first, second);
+    // }
+    // }
+}
