445eceeda772095e5049e118e1c7098d40be333e
Dev Code <hellodev_code@outlook.com>
2024-10-16 23:04:51 +0530

    updated the Template

--- Template.java
+++ Template.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-// import java.math.*;
 // import static java.lang.Math.min;
 // import static java.lang.Math.max;
 // import static java.lang.Math.abs;
@@ -30,51 +29,47 @@ public class Template {
         return st.nextToken();
     }
 
-    // private static long sqrt(long x) {
-    // return new BigInteger(String.valueOf(x)).sqrt().longValue();
-    // }
-
     // private static long gcd(long a, long b) {
-    // while (b != 0) {
-    // long temp = b;
-    // b = a % b;
-    // a = temp;
-    // }
-    // return a;
+    //     while (b != 0) {
+    //         long temp = b;
+    //         b = a % b;
+    //         a = temp;
+    //     }
+    //     return a;
     // }
 
     // private static long lcm(long a, long b) {
-    // return a * b / gcd(a, b);
+    //     return a * b / gcd(a, b);
     // }
 
     // static class Pair implements Comparable<Pair> {
-    // long first;
-    // long second;
+    //     long first;
+    //     long second;
     //
-    // Pair(long _first, long _second) {
-    // this.first = _first;
-    // this.second = _second;
-    // }
-    // @Override
-    // public int compareTo(Pair o) {
-    // return Long.compare(this.first, o.first);
-    // }
-    // @Override
-    // public String toString() {
-    // return "(" + first + ',' + second + ")";
-    // }
-    // @Override
-    // public boolean equals(Object o) {
-    // if (this == o)
-    // return true;
-    // if (o == null || getClass() != o.getClass())
-    // return false;
-    // Pair pair = (Pair) o;
-    // return first == pair.first && second == pair.second;
-    // }
-    // @Override
-    // public int hashCode() {
-    // return Objects.hash(first, second);
-    // }
+    //     Pair(long _first, long _second) {
+    //         this.first = _first;
+    //         this.second = _second;
+    //     }
+    //     @Override
+    //     public int compareTo(Pair o) {
+    //         return Long.compare(this.first, o.first);
+    //     }
+    //     @Override
+    //     public String toString() {
+    //         return "(" + first + ',' + second + ")";
+    //     }
+    //     @Override
+    //     public boolean equals(Object o) {
+    //         if (this == o)
+    //             return true;
+    //         if (o == null || getClass() != o.getClass())
+    //             return false;
+    //         Pair pair = (Pair) o;
+    //         return first == pair.first && second == pair.second;
+    //     }
+    //     @Override
+    //     public int hashCode() {
+    //         return Objects.hash(first, second);
+    //     }
     // }
 }
