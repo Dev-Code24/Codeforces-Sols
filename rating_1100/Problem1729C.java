902827dd860d92b6f13484b2cadea374dd1ecb18
Dev Code <hellodev_code@outlook.com>
2024-11-01 18:16:29 +0530

    01 Nov

---
+++ rating_1100/Problem1729C.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Math.abs;
+
+public class Problem1729C {
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int T = Integer.parseInt(nextToken());
+
+        while (T-- > 0) {
+            String s = nextToken();
+            TreeMap<Character, TreeSet<Integer>> index = new TreeMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                index.computeIfAbsent(s.charAt(i), k -> new TreeSet<>()).add(i + 1);
+            }
+
+            char start = s.charAt(0), end = s.charAt(s.length() - 1);
+            int cost = abs(end - start), count = 0;
+            StringBuilder path = new StringBuilder();
+
+            if (start <= end) {
+                for (char c = start; c <= end; c++) {
+                    if (index.containsKey(c)) {
+                        count += index.get(c).size();
+                        for (int pos : index.get(c)) {
+                            path.append(pos).append(" ");
+                        }
+                    }
+                }
+            } else {
+                for (char c = start; c >= end; c--) {
+                    if (index.containsKey(c)) {
+                        count += index.get(c).size();
+                        for (int pos : index.get(c)) {
+                            path.append(pos).append(" ");
+                        }
+                    }
+                }
+            }
+
+            out.println(cost + " " + count);
+            out.println(path.toString().trim());
+        }
+
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    private static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+}
