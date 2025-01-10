d0ccb5593e6610d7b8572a3794e21d854406e26a
Dev Code <hellodev_code@outlook.com>
2025-01-10 22:21:14 +0530

    10 Jan 2025

--- rating_900/Problem1883B.java
+++ rating_900/Problem1883B.java
@@ -1,34 +1,49 @@
+import java.io.*;
 import java.util.*;
+import static java.lang.Integer.parseInt;
+// import static java.lang.Math.min;
+// import static java.lang.Math.max;
+// import static java.lang.Math.abs;
 
 public class Problem1883B {
-    // bacacd
-    private static void solve(Scanner sc) {
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        sc.nextLine();
-        String s = sc.nextLine();
-        Map<Character, Integer> map = new HashMap<>();
-        int char_with_odd_occurence = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int TC = parseInt(nextToken());
+        while (TC-- > 0) {
+            int n = parseInt(nextToken()), k = parseInt(nextToken());
+            char s[] = nextToken().toCharArray();
+
+            int arr[] = new int[26];
+            for (int i = 0; i < n; i++)
+                arr[s[i] - 'a']++;
+
+            int cnt = 0;
+            for (int e : arr)
+                if ((e & 1) == 1)
+                    cnt++;
+
+            if (cnt > k + 1) {
+                out.println("NO");
+            } else {
+                out.println("YES");
+            }
         }
-        for (Character ch : map.keySet()) {
-            if (map.get(ch) % 2 != 0)
-                char_with_odd_occurence++;
-        }
-        if (char_with_odd_occurence - k > 1)
-            System.out.println("NO");
-        else
-            System.out.println("YES");
+
+        out.flush();
+        out.close();
+        br.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            solve(sc);
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
         }
-        sc.close();
+        return st.nextToken();
     }
 }
