193b3670a6652fa93411a22129445923223dcec0
Dev Code <hellodev_code@outlook.com>
2025-01-19 00:54:17 +0530

    19 Jan 2025

--- rating_1200/Problem1848B.java
+++ rating_1200/Problem1848B.java
@@ -1,95 +1,69 @@
+import java.io.*;
 import java.util.*;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Integer.parseInt;
+// import static java.lang.Math.abs;
 
 public class Problem1848B {
-    static int n, k;
-    static int c[];
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
 
-    public static void solve(Scanner sc) {
-        n = sc.nextInt();
-        k = sc.nextInt();
+    static int n, k, c[];
 
-        c = new int[n];
-        for (int i = 0; i < n; i++) {
-            c[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws IOException {
 
-        int lastEncntr[] = new int[k];
-        Arrays.fill(lastEncntr, -1);
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int TC = parseInt(nextToken());
+        while (TC-- > 0) {
+            n = parseInt(nextToken());
+            k = parseInt(nextToken());
+            c = new int[n];
+            for (int i = 0; i < n; i++) c[i] = parseInt(nextToken());
 
-        int max1_step[], max2_step[];
-        // stores the largest distance bw consecutive occurence of c[i]
-        max1_step = new int[k];
-        // stores the second largest distance bw consecutive occurence of c[i]
-        max2_step = new int[k];
+            int last[] = new int[k];
+            Arrays.fill(last, -1);
+            int max1[] = new int[k], max2[] = new int[k];
+            for (int i = 0; i < n; i++) {
+                int tt = i - last[c[i] - 1];
+                if (max1[c[i] - 1] < tt) {
+                    max2[c[i] - 1] = max1[c[i] - 1];
+                    max1[c[i] - 1] = tt;
+                } else if (tt > max2[c[i] - 1]) {
+                    max2[c[i] - 1] = tt;
+                }
 
-        for (int i = 0; i < n; ++i) {
-            int step = i - lastEncntr[c[i] - 1];
-
-            if (step > max1_step[c[i] - 1]) {
-                max2_step[c[i] - 1] = max1_step[c[i] - 1];
-                max1_step[c[i] - 1] = step;
-            } else if (step > max2_step[c[i] - 1]) {
-                max2_step[c[i] - 1] = step;
+                last[c[i] - 1] = i;
+            }
+            
+            for (int i = 0; i < k; i++) {
+                int tt = n - last[i];
+                if (tt > max1[i]) {
+                    max2[i] = max1[i];
+                    max1[i] = tt;
+                } else if (max2[i] < tt) {
+                    max2[i] = tt;
+                }
             }
 
-            lastEncntr[c[i] - 1] = i;
-        }
-
-        for (int i = 0; i < k; ++i) {
-            int step = n - lastEncntr[i];
-
-            if (step > max1_step[i]) {
-                max2_step[i] = max1_step[i];
-                max1_step[i] = step;
-            } else if (step > max2_step[i]) {
-                max2_step[i] = step;
+            int ans = (int) 1e9;
+            for (int i = 0; i < k; i++) {
+                ans = min(ans, max(max2[i], (max1[i] + 1 )/ 2));
             }
+            out.println(ans - 1);
         }
-        int ans = (int) 1e9;
-        for (int i = 0; i < k; ++i) {
-            ans = Math.min(ans, Math.max((max1_step[i] + 1) / 2, max2_step[i]));
-        }
-        System.out.println(ans - 1);
 
+        out.flush();
+        out.close();
+        br.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            solve(sc);
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
         }
-        sc.close();
+        return st.nextToken();
     }
-
-    // private static long gcd(long a, long b) {
-    // while (b != 0) {
-    // long temp = b;
-    // b = a % b;
-    // a = temp;
-    // }
-    // return a;
-    // }
-
-    // private static long lcm(long a, long b) {
-    // return a * b / gcd(a, b);
-    // }
-
-    // static class Pair implements Comparable<Pair> {
-    // long first;
-    // long second;
-    //
-    // Pair(long _first, long _second) {
-    // this.first = _first;
-    // this.second = _second;
-    // }
-    // @Override
-    // public int compareTo(Pair o) {
-    // return Long.compare(this.first, o.first);
-    // }
-    // @Override
-    // public String toString() {
-    // return "(" + first + ',' + second + ")";
-    // }
-    // }
 }
