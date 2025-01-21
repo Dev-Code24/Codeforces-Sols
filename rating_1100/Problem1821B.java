2f93640f58c8ecc5fb9e8ac47d44dbea23173b6f
Dev Code <hellodev_code@outlook.com>
2025-01-21 21:32:02 +0530

    removed Problem2036D from Round-984

--- rating_1100/Problem1821B.java
+++ rating_1100/Problem1821B.java
@@ -1,75 +1,44 @@
+import java.io.*;
 import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
 public class Problem1821B {
-    static int n;
-    static int a[], b[];
-    static int left, right;
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
 
-    public static void solve(Scanner sc) {
-        n = sc.nextInt();
-        a = new int[n];
-        b = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            b[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int TC = parseInt(nextToken());
+        while (TC-- > 0) {
+            int n = parseInt(nextToken());
+            int a[] = new int[n], b[] = new int[n];
+            for(int i = 0; i < n; i++) a[i] = parseInt(nextToken());
+            for(int i = 0; i < n; i++) b[i] = parseInt(nextToken());
+
+            int l = 0, r = n - 1;
+
+            while(l < n && a[l] == b[l]) l++;
+            while(l - 1 >= 0 && b[l] >= b[l - 1]) l--;
+
+            while(r >= 0 && a[r] == b[r]) r--;
+            while(r + 1 < n && b[r] <= b[r + 1]) r++;
+
+            out.println(l + 1 + " " + (r + 1));
         }
 
-        left = 0;
-        right = n - 1;
-
-        while (a[left] == b[left]) {
-            left++;
-        }
-        while (a[right] == b[right]) {
-            right--;
-        }
-        while (left > 0 && b[left - 1] <= b[left]) {
-            left--;
-        }
-        while (right < n - 1 && b[right + 1] >= b[right]) {
-            right++;
-        }
-        System.out.println((left + 1) + " " + (right + 1));
+        out.flush();
+        out.close();
+        br.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            solve(sc);
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
         }
-        sc.close();
+        return st.nextToken();
     }
-
-    // private static long gcd(long a, long b) {
-    // while (b != 0) {
-    // long temp = b;
-    // b = a % b;
-    // a = temp;
-    // }
-    // return a;
-    // }
-
-    // private static long lcm(long a, long b) {
-    // return a * b / gcd(a, b);
-    // }
-
-    // static class Pair implements Comparable<Pair> {
-    // long first;
-    // long second;
-    //
-    // Pair(long _first, long _second) {
-    // this.first = _first;
-    // this.second = _second;
-    // }
-    // @Override
-    // public int compareTo(Pair o) {
-    // return Long.compare(this.first, o.first);
-    // }
-    // @Override
-    // public String toString() {
-    // return "(" + first + ',' + second + ")";
-    // }
-    // }
 }
