92d5cfe970f7d24a728fcb1416d0d24de78a523b
Dev Code <hellodev_code@outlook.com>
2025-01-22 15:41:43 +0530

    22 Jan 2025

--- Contests-2024/Pinely-round-4-div-1-div-2/Problem1991C.java
+++ Contests-2024/Pinely-round-4-div-1-div-2/Problem1991C.java
@@ -1,77 +1,64 @@
+import java.io.*;
 import java.util.*;
 
+import static java.lang.Math.abs;
+import static java.lang.Integer.parseInt;
+
 public class Problem1991C {
-    static int n;
-    static int a[];
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
 
-    public static void solve(Scanner sc) {
-        n = sc.nextInt();
-        a = new int[n + 1];
-        boolean hasOdd = false, hasEven = false;
-        for (int i = 1; i <= n; i++) {
-            a[i] = sc.nextInt();
-            if (a[i] % 2 == 1) {
-                hasOdd = true;
-            } else {
-                hasEven = true;
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int TC = parseInt(nextToken());
+        while (TC-- > 0) {
+            int n = parseInt(nextToken());
+            long a[] = new long[n];
+            boolean odd = false, even = false;
+            for (int i = 0; i < n; i++) {
+                a[i] = parseInt(nextToken());
+                if(a[i] % 2 == 1) odd = true;
+                if(a[i] % 2 == 0) even = true;
             }
-        }
-        if (hasEven && hasOdd) {
-            System.out.println(-1);
-        } else {
-            List<Integer> operations = new ArrayList<>();
-            for (int i = 29; i >= 0; i--) {
-                operations.add(1 << i);
+
+            if(odd && even) {
+                out.println(-1);
+                continue;
             }
-            if (hasEven) {
-                operations.add(1);
+
+            Queue<Integer> q = new LinkedList<>();
+
+            for(int i = 29; i >= 0; i--) {
+                for(int j = 0; j < n; j++) a[j] = abs(a[j] -(long) (1 << i));
+                q.offer((1 << i));
             }
-            System.out.println(operations.size());
-            for (int x : operations) {
-                System.out.print(x + " ");
+
+            long sum = Arrays.stream(a).sum();
+            if(sum == n) {
+                q.offer(1);
+                out.println(31);
+            }else {
+                out.println(30);
             }
-            System.out.println();
+
+            for(long el : q) {
+                out.print(el + " ");
+            }
+            out.println();
         }
 
+        out.flush();
+        out.close();
+        br.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            solve(sc);
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
         }
-        sc.close();
+        return st.nextToken();
     }
-
-    // private static long gcd(long a, long b) {
-    // while (b != 0) {
-    // long temp = b;
-    // b = a % b;
-    // a = temp;
-    // }
-    // return a;
-    // }
-
-    // private static long lcm(long a, long b) {
-    // return a * b / gcd(a, b);
-    // }
-
-    // static class Pair implements Comparable<Pair> {
-    // long first;
-    // long second;
-    //
-    // Pair(long _first, long _second) {
-    // this.first = _first;
-    // this.second = _second;
-    // }
-    // @Override
-    // public int compareTo(Pair o) {
-    // return Long.compare(this.first, o.first);
-    // }
-    // @Override
-    // public String toString() {
-    // return "(" + first + ',' + second + ")";
-    // }
-    // }
 }
