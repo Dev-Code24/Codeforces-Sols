18e9321eaf704a71900739ea912f096781d973de
Dev Code <hellodev_code@outlook.com>
2025-01-25 15:13:24 +0530

    corrected Template code

--- Template.java
+++ Template.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+
 import static java.lang.Integer.parseInt;
 // import static java.lang.Math.min;
 // import static java.lang.Math.max;
@@ -32,52 +33,55 @@ public class Template {
         return st.nextToken();
     }
 
-    // static long gcd(long a, long b) {
-    //     while (b != 0) {
-    //         long temp = b;
-    //         b = a % b;
-    //         a = temp;
-    //     }
-    //     return a;
-    // }
-
-    // static long lcm(long a, long b) {
-    //     return a * b / gcd(a, b);
-    // }
-
-    // static class Pair implements Comparable<Pair> {
-    //     long first;
-    //     long second;
-    //
-    //     Pair() {
-    //         this.first = 0L;
-    //         this.second = 0L;
-    //     }
-
-    //     Pair(long _first, long _second) {
-    //         this.first = _first;
-    //         this.second = _second;
-    //     }
-    //     @Override
-    //     public int compareTo(Pair o) {
-    //         return Long.compare(this.first, o.first);
-    //     }
-    //     @Override
-    //     public String toString() {
-    //         return "(" + first + ',' + second + ")";
-    //     }
-    //     @Override
-    //     public boolean equals(Object o) {
-    //         if (this == o)
-    //             return true;
-    //         if (o == null || getClass() != o.getClass())
-    //             return false;
-    //         Pair pair = (Pair) o;
-    //         return first == pair.first && second == pair.second;
-    //     }
-    //     @Override
-    //     public int hashCode() {
-    //         return Objects.hash(first, second);
-    //     }
-    // }
+//     static long gcd(long a, long b) {
+//         while (b != 0) {
+//             long temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
+//         return a;
+//     }
+//
+//     static long lcm(long a, long b) {
+//         return a * b / gcd(a, b);
+//     }
+//
+//     static class Pair implements Comparable<Pair> {
+//         long first;
+//         long second;
+//
+//         Pair() {
+//             this.first = 0L;
+//             this.second = 0L;
+//         }
+//
+//         Pair(long _first, long _second) {
+//             this.first = _first;
+//             this.second = _second;
+//         }
+//         @Override
+//         public int compareTo(Pair o) {
+//             if (this.first != o.first) {
+//                 return Long.compare(this.first, o.first);
+//             }
+//             return Long.compare(this.second, o.second);
+//         }
+//         @Override
+//         public String toString() {
+//             return "(" + first + ',' + second + ")";
+//         }
+//         @Override
+//         public boolean equals(Object o) {
+//             if (this == o)
+//                 return true;
+//             if (o == null || getClass() != o.getClass())
+//                 return false;
+//             Pair pair = (Pair) o;
+//             return first == pair.first && second == pair.second;
+//         }
+//         @Override
+//         public int hashCode() {
+//             return Objects.hash(first, second);
+//         }
+//     }
 }
