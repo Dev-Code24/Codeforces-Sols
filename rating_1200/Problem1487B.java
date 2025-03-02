2ca20f8289c8288047c226e25880038e3dbf86fb
Dev Code <hellodev_code@outlook.com>
2025-03-02 16:24:17 +0530

    2 Mar 2025

--- rating_1200/Problem1487B.java
+++ rating_1200/Problem1487B.java
@@ -1,68 +1,40 @@
+import java.io.*;
 import java.util.*;
 
+import static java.lang.Integer.parseInt;
+// import static java.lang.Math.min;
+// import static java.lang.Math.max;
+// import static java.lang.Math.abs;
+
 public class Problem1487B {
-    static long n, k;
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+    // Avoid Map for frequency based operations, instead use Arrays for significant improvement in Time Complexity
 
-    public static void main(String[] args) {
+    static void solve() throws IOException {
+        int n = parseInt(nextToken()), k = parseInt(nextToken());
+        k--;
 
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            n = sc.nextInt();
-            k = sc.nextInt();
-
-            k--;
-            long floor = n / 2;
-            if ((n & 1) == 0) {
-                System.out.println(k % n + 1);
-            } else {
-                System.out.println((k + (k / floor)) % n + 1);
-            }
-        }
-        sc.close();
+        if((n & 1) == 0) out.println(k % n + 1);
+        else out.println((k + (k / (n / 2))) % n + 1);
     }
 
-    // private static long gcd(long a, long b) {
-    // while (b != 0) {
-    // long temp = b;
-    // b = a % b;
-    // a = temp;
-    // }
-    // return a;
-    // }
+    public static void main(String[] args) throws IOException {
 
-    // private static long lcm(long a, long b) {
-    // return a * b / gcd(a, b);
-    // }
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int TC = parseInt(nextToken());
+        while (TC-- > 0) solve();
+        out.flush();
+        out.close();
+        br.close();
+    }
 
-    // static class Pair implements Comparable<Pair> {
-    // long first;
-    // long second;
-    //
-    // Pair(long _first, long _second) {
-    // this.first = _first;
-    // this.second = _second;
-    // }
-    // @Override
-    // public int compareTo(Pair o) {
-    // return Long.compare(this.first, o.first);
-    // }
-    // @Override
-    // public String toString() {
-    // return "(" + first + ',' + second + ")";
-    // }
-    // @Override
-    // public boolean equals(Object o) {
-    // if (this == o)
-    // return true;
-    // if (o == null || getClass() != o.getClass())
-    // return false;
-    // Pair pair = (Pair) o;
-    // return first == pair.first && second == pair.second;
-    // }
-    // @Override
-    // public int hashCode() {
-    // return Objects.hash(first, second);
-    // }
-    // }
+    static String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
 }
